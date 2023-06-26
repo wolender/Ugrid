34c9ab5330869d035c516a16706d0ed563f0d240
wolender <wolender@griddynamics.com>
2023-06-26 15:49:37 +0200

    refractoring task1

--- python_scripts/task1/filename.py
+++ python_scripts/task1/filename.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-#Create a script that accepts the file name and puts its extension to output.
-#If there is no extension - an exception should be raised.
+"""
+Create a script that accepts the file name and puts its extension to output.
+If there is no extension - an exception should be raised.
+"""
 
 import argparse
-
 
 parser = argparse.ArgumentParser(description='What the program does')
 parser.add_argument('FILENAME', help="Provide a filename")
 
 args = parser.parse_args()
 
-filename=str(args.FILENAME)
+FILENAME=str(args.FILENAME)
 
 #spliting the filename in to a list of striings where . is the separator,
 # 1 for maxssplit to take multi doted extesions like .noarch.rpm
-filelist=filename.split('.',1) 
-
+filelist=FILENAME.split('.',1)
 
 if len(filelist) < 2:
     raise ValueError("ERROR File does not have an extension")
-else:
-    print(f"The extension of the file is: {filelist[1]}")
+print(f"The extension of the file is: {filelist[1]}")
--- python_scripts/task6/apicall.py
+++ python_scripts/task6/apicall.py
@@ -1,5 +1,77 @@
 #!/usr/bin/env python3
 
+"""
+Practical tasks (Part 2)
+
+Create a script that uses the Survey Monkey (https://www.surveymonkey.com) service, creates a survey, and sends an invitation to go through it.
+
+
+PREREQUISITES:
+
+Sign up at https://www.surveymonkey.com 
+
+Create a draft application at https://developer.surveymonkey.com   
+
+No need to deploy your application. It's just for testing. Do not forget to set permissions for your application.
+
+After creating a draft application you will obtain an ACCESS_TOKEN which is needed to do API requests from your script.
+
+
+REQUIREMENTS:
+
+The script should accept a JSON file with questions for the survey and a text file with a list of email addresses.
+
+The structure of a JSON file with questions:
+
+{
+
+   "Survey_Name": {
+
+      "Page_Name": {
+
+          "Question1_Name": {
+
+              "Description" : "Description of question",
+
+              "Answers" : [
+
+                  "Answer1",
+
+                  "Answer2",
+
+                  "Answer3"
+
+              ]
+
+          },
+
+          "Question2_Name": {
+
+              "Description" : "Description of question",
+
+              "Answers" : [
+
+                  "Answer1",
+
+                  "Answer2",
+
+                  "Answer3"
+
+              ]
+
+          }
+
+          . . .
+
+      }
+
+   }
+
+}
+
+There should be at least 3 questions and 2 recipients.
+"""
+
 import requests
 
 url="https://developer.surveymonkey.com/apps/109210/"
