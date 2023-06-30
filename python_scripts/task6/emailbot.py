22b0e7eea7b968633bafda659e92632e7c82e257
wolender <wolender@griddynamics.com>
2023-06-30 13:35:37 +0200

    credentials cleanup

--- python_scripts/task6/emailbot.py
+++ python_scripts/task6/emailbot.py
@@ -32,7 +32,7 @@ def gmail_send_message(content,email):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '/Users/wolender/Google/cred.json', SCOPES)
+                'cred.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
