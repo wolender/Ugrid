487ad4a8500075e27039f89bb082e25d03e93cd0
wolender <wolender@griddynamics.com>
2023-07-21 13:14:40 +0200

    fix task 3 script returns user agents instead of ip addresses

--- python_scripts/task3/access.py
+++ python_scripts/task3/access.py
@@ -10,6 +10,7 @@ example:
 '''
 
 import argparse
+import re
 
 parser=argparse.ArgumentParser(description="""
 Displays total nmber of different user agents and number of requests.
@@ -18,19 +19,34 @@ parser.add_argument("filename",help="Logfile name")
 
 args=parser.parse_args()
 dict_of_agents={}
+EXPR = r'"([^"]+)"$'
+"""
+maches and any number of characters in " " that
+- are not a quote
+- quoted characters ale at the end of the line $
+"""
 with open (args.filename, 'r',encoding="UTF-8") as f:
     while True:
         #getting the ip address
-        agent=f.readline().split(" ",maxsplit=1)[0] #using readline for efficiency
+        line=f.readline() #using readline for efficiency
+        agent = re.search(EXPR, line)
+
         if not agent: #exit the loop if agent is empty
             break
         #getting uniqe ones
-        if agent not in dict_of_agents:
-            dict_of_agents.setdefault(agent,1)
+        if agent.group(1) == "-":
+            continue
+        if agent.group(1) not in dict_of_agents:
+            dict_of_agents.setdefault(agent.group(1),1)
         else:
-            dict_of_agents[agent] +=1       #if already in the dictionary add +1 to request count
-print(f"Total number of different user agents: {len(dict_of_agents.keys())}")
+            #if already in the dictionary add +1 to request count
+            dict_of_agents[agent.group(1)] +=1
 
+print(f"""
+=====================================================================
+Total number of different user agents: {len(dict_of_agents.keys())}
+=====================================================================
+""")
 
 for agent, COUNT in dict_of_agents.items():
     print(f"Agent: {agent} had {COUNT} requests")
